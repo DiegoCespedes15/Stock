abb45404cb32a118b16f1ba4b84550aa68aba2eb
Diego Cespedes <104471405+DiegoCespedes15@users.noreply.github.com>
2025-08-10 14:52:35 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,12 +2,13 @@
 
 import customtkinter as ctk
 from modulos import stock
+from modulos import ventas
 
 
-def abrir_dashboard():
+def abrir_dashboard(nombre_usuario):
     app = ctk.CTk()
     app.title("Sistema de Inventario Inteligente")
-    app.geometry("900x600")
+    app.geometry("1280x720")
 
     ctk.set_appearance_mode("Light")
     ctk.set_default_color_theme("blue")
@@ -17,28 +18,31 @@ def abrir_dashboard():
 
     contenido_frame = ctk.CTkFrame(app, corner_radius=0)
     contenido_frame.pack(side="right", expand=True, fill="both")
-    
+
     def limpiar_contenido():
         for widget in contenido_frame.winfo_children():
             widget.destroy()
 
     def mostrar_inicio():
         limpiar_contenido()
-        ctk.CTkLabel(contenido_frame, text="Bienvenido al Sistema", font=("Arial", 20)).pack(pady=30)
+        ctk.CTkLabel(
+            contenido_frame, 
+            text=f"Bienvenido, {nombre_usuario}",
+            font=("Arial", 20, "bold")
+        ).pack(pady=30)
 
-
-    # Funciones para cada módulo
     def mostrar_modulo(nombre):
-        for widget in contenido_frame.winfo_children():
-            widget.destroy()
+        limpiar_contenido()
+        ctk.CTkLabel(
+            contenido_frame, 
+            text=f"Módulo: {nombre}", 
+            font=("Arial", 20)
+        ).pack(pady=30)
 
-        ctk.Label(contenido_frame, text=f"Módulo: {nombre}", font=("Arial", 20)).pack(pady=30)
-
-    # Botones del menú
     botones = [
-        ("Inicio", lambda: mostrar_modulo("Inicio")),
-        ("Productos", lambda: stock.mostrar_productos( contenido_frame)),
-        ("Ventas", lambda: mostrar_modulo("Ventas")),
+        ("Inicio", mostrar_inicio),
+        ("Productos", lambda: stock.mostrar_productos(contenido_frame)),
+        ("Ventas", lambda: ventas.mostrar_ventas(contenido_frame)),
         ("Reportes", lambda: mostrar_modulo("Reportes")),
         ("Salir", app.destroy)
     ]
